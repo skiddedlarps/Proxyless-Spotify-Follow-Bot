132f66f4a306d851d3ee837f8a8fe6dade619960
Qezy <102873203+itsqezy@users.noreply.github.com>
2022-05-07 22:55:59 -0700

    Update follow_bot.py

--- follow_bot.py
+++ follow_bot.py
@@ -16,7 +16,7 @@ class spotify:
     def register_account(self):
         headers = {
             "Accept": "*/*",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+            "Qezy": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
             "Content-Type": "application/x-www-form-urlencoded",
             "Referer": "https://www.spotify.com/"
         }
